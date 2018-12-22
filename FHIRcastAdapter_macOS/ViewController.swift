7fafe66b2f3366d65e9bfb9e3bb9552843c39da6
Martin Bellehumeur <martin_bellehumeur@yahoo.com>
2018-12-22 15:43:49 +0100

    safekeeping

--- FHIRcastAdapter_macOS/ViewController.swift
+++ FHIRcastAdapter_macOS/ViewController.swift
@@ -3,7 +3,6 @@
 //  FHIRcastAdapter_macOS
 //
 //  Created by Martin Bellehumeur on 21.12.18.
-//  Copyright © 2018 Martin Bellehumeur. All rights reserved.
 //
 
 import Cocoa
@@ -11,14 +10,14 @@ import Cocoa
 class ViewController: NSViewController {
 
     let defaults = UserDefaults.standard
-
-    @IBAction func subscribeClick(_ sender: Any) { subscribe(mode: "subscribe") }
-    @IBAction func unsubscribeClick(_ sender: Any) { subscribe(mode: "unsubscribe")}
     
-    @IBOutlet weak var logView: NSScrollView!
     @IBOutlet weak var hubURL: NSTextField!
     @IBOutlet weak var Secret: NSTextField!
+    @IBOutlet weak var topic: NSTextField!
     
+    @IBAction func subscribeClick(_ sender: Any) { subscribe(mode: "subscribe") }
+    @IBAction func unsubscribeClick(_ sender: Any) { subscribe(mode: "unsubscribe")}
+
     @IBOutlet weak var open_patient_chart: NSButton!
     @IBOutlet weak var switch_patient_chart: NSButton!
     @IBOutlet weak var close_patient_chart: NSButton!
@@ -28,12 +27,11 @@ class ViewController: NSViewController {
     @IBOutlet weak var logout_user: NSButton!
     @IBOutlet weak var hibernate_user: NSButton!
     
-    func log(msg: String){
-       
-        logView.insertNewline(msg)
-    }
+    @IBOutlet var logTextView: NSTextView!
+
+    func log(msg: String){ logTextView.string += msg + "\n" }
     
-    func subscribe(mode: String) {
+    func subscribe(mode: String)  {
         
         let subscribeURL = URL(string: hubURL.stringValue)!
         let session = URLSession.shared
@@ -51,32 +49,42 @@ class ViewController: NSViewController {
         if (close_imaging_study.state == .on)  { eventList += "close-imaging-study,"}
         if (hibernate_user.state == .on)       { eventList += "hibernate-user,"}
         if (logout_user.state == .on)          { eventList += "logout-user,"}
+        if eventList.suffix(1) == "," { eventList.removeLast() }
         
         postString += "&hub.events=" + eventList
-        postString += "&hub.secret=secert"
-        postString += "&hub.topic=12345"
+        postString += "&hub.secret=" + Secret.stringValue
+        postString += "&hub.topic=" + topic.stringValue
         postString += "&hub.lease=999"
         postString += "&hub.channel.type=websocket"
-        postString += "&hub.channel.endpoint=12345"
-        log(msg: "Subscribing")
+        postString += "&hub.channel.endpoint="  + topic.stringValue
+        log(msg: "Subscribing to " + hubURL.stringValue + " with events: " + eventList)
+        var responseString = ""
         request.httpBody = postString.data(using: .utf8)
         let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+            guard let data = data, error == nil else {
+                // check for fundamental networking error
                 print("error = \(String(describing: error))")
                 return
             }
-            
-            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-                print("statusCode should be 200, but is \(httpStatus.statusCode)")
-                print("response = \(String(describing: response))")
-            }
-            
-            let responseString = String(data: data, encoding: .utf8)
-            print("responseString = \(String(describing: responseString))")
+            //print("response = \(String(describing: response))")
+            responseString = ""
+            print(responseString)
+       //     self.log(msg: String(describing: response))
         })
-        
         task.resume()
-
+        // connect websocket
+        var wsURL="ws"
+        if hubURL.stringValue.prefix(5) == "https" {
+            wsURL += "s" + hubURL.stringValue.suffix( hubURL.stringValue.count - 5)
+        }
+        else {
+            wsURL += hubURL.stringValue.suffix( hubURL.stringValue.count - 4)
+        }
+        if wsURL.suffix(1) == "/" {
+            wsURL += "bind/" + topic.stringValue
+        }
+        else { wsURL += "/bind/" + topic.stringValue }
+        print(wsURL)
     }
     
     override func viewDidLoad() {
@@ -98,8 +106,9 @@ class ViewController: NSViewController {
         else { hibernate_user.state = .off }
         if (defaults.bool(forKey: "logout-user")) { logout_user.state = .on}
         else { logout_user.state = .off }
-        hubURL.stringValue = defaults.string(forKey: "hubURL") ?? "enter url"
-        
+        hubURL.stringValue = defaults.string(forKey: "hubURL") ?? "http://localhost:3000/api/hub/"
+        Secret.stringValue = defaults.string(forKey: "Secret") ?? "secret"
+        topic.stringValue = randomString(length: 12)
     }
     
     @IBAction func saveSettingsClick(_ sender: Any) {
@@ -120,7 +129,13 @@ class ViewController: NSViewController {
         if (logout_user.state == .on)   { defaults.set(true, forKey: "logout-user") }
           else { defaults.set(false, forKey: "logout-user") }
         defaults.set(hubURL.stringValue, forKey: "hubURL")
+        defaults.set(Secret.stringValue, forKey: "Secret")
         
+    }
+    
+    func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0...length-1).map{ _ in letters.randomElement()! })
     }
     
     override var representedObject: Any? {
